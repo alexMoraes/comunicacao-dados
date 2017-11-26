fa829f309fef7c47c9e5c76efb6b79d0c894a0ca
Gabriel Martins <gabrielnmartins@gmail.com>
2017-11-26 21:21:01 -0200

    fix no sort da matriz H

--- getOrder.m
+++ getOrder.m
@@ -1,10 +1,11 @@
 function y = getOrder(x)
+    % Retorna a ordem das colunas baseada em suas normas
     [~, c] = size(x);
     a = zeros(c, 2);
     for i = 1:c
         a(i, 1) = norm(x(:, i));
         a(i, 2) = i;
     end
-    s = sortrows(a, -1);
+    s = sortrows(a, 1);
     y = s(:, 2);
 end
