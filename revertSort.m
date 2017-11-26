f42d36e272fa5f74abf78bd2f299963693b4dee9
Gabriel Martins <gabrielnmartins@gmail.com>
2017-11-26 21:22:06 -0200

    documentação de funções

--- revertSort.m
+++ revertSort.m
@@ -1,4 +1,5 @@
 function y = revertSort(x, o)
+    % Desfazem o ordenamento de `x` baseado na ordem `o`
     l = length(x);
     y = complex(zeros(l, 1), 0);
     for i = 1:l
--- sortMatrix.m
+++ sortMatrix.m
@@ -1,4 +1,5 @@
 function y = sortMatrix(x, o)
+    % Ordena as colunas de `x` baseada na ordem `o`
     [l, c] = size(x);
     y = zeros(l, c);
     for i = 1:c
